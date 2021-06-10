8d96778f530d8d8fac5d5974e6c18046c9def590
shushushushushu <15731115415@163.com>
2021-06-10 12:59:53 +0800

    Create Client.php

---
+++ src/Client.php
@@ -0,0 +1,19 @@
+<?php
+namespace Shu;
+
+/**
+ * 
+ */
+class Client
+{
+	
+	function __construct()
+	{
+		echo 'start' . PHP_EOL;
+	}
+
+	public function run()
+	{
+		echo 'exec run function!';
+	}
+}
